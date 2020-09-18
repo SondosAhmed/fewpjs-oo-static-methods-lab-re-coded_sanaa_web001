6b74c4306045306cf1a7885a0c328b95dcfa1691
Sondos Ahmed <sondosalshami@gmail.com>
2020-09-18 08:15:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ class Formatter {
     let superWord="";
         for (let i = 0; i < wordArray.length; i++) {
           if (i==0)
+<<<<<<< HEAD
           superWord += Formatter.capitalize(wordArray[i])+' ';
+=======
+          superWord += Formatter.capitalize(wordArray[i])+' ';}
+>>>>>>> 34cc6d4f9362e5a7e955ce237a81f6d5f17e70d0
         else  if( wordArray[i]=="the"||  wordArray[i]=="a"|| wordArray[i]== "an"||  wordArray[i]=="but"||  wordArray[i]=="of"|| wordArray[i]== "and"|| wordArray[i]== "for"|| wordArray[i]== "at"||  wordArray[i]=="by"|| wordArray[i]== "from")
           superWord+=wordArray[i]+' ';
 
